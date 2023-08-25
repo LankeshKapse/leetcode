08d348edbf4c78d9552304eaf3ac9a1bb0eb8f0a
a818517 <lkapse63@gmail.com>
2023-08-25 13:40:13 +0530

    leetcode stack demo

---
+++ src/main/java/org/lucky/leetcode/ds/Stack.java
@@ -0,0 +1,67 @@
+package org.lucky.leetcode.ds;
+
+import java.lang.reflect.Array;
+import java.util.Objects;
+
+public class Stack<T> {
+
+    private final T[] finite_stack ;
+    private int push_pointer=-1;
+    private int pop_pointer=-1;
+
+    public Stack(Class<T> clazz) {
+        this.finite_stack = (T[]) Array.newInstance(clazz, 5);
+    }
+
+    public void push(T item){
+        try{
+            finite_stack[++push_pointer]=item;
+            pop_pointer=push_pointer;
+        }catch (ArrayIndexOutOfBoundsException ex){
+            throw new RuntimeException("Stack full");
+        }
+
+
+    }
+
+    public T pop(){
+        T item = finite_stack[pop_pointer];
+        if(Objects.isNull(item))
+            throw new RuntimeException("No item left");
+        finite_stack[pop_pointer--]=null;
+        push_pointer--;
+        return item;
+    }
+
+    public void print(){
+        for(T item: finite_stack){
+            if(Objects.isNull(item))
+                continue;
+            System.out.print(item+",");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(Integer.class);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.print();
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.pop();
+        stack.push(9);
+        stack.print();
+        stack.push(10);
+        stack.print();
+    }
+
+}
